7ebcc34015c8252e30fac22c18bba9986c14042e
Taras Romaniuk <tarasrom25@gmail.com>
2024-05-26 08:17:00 +0300

    delete user adverts if user deleted & added db seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // create admin
+  const admin = await prisma.user.create({
+    data: {
+      name: "Tom",
+      phone: "0981234323",
+      email: "tarasromaniuk555@gmail.com",
+      password: "$2b$10$4V9lz1TPB5Tb6dYOK./W4eA5pnFm4uLAKEb.moQIk2Z.CP5Vu2ttC",
+      isVerified: true,
+      role: "admin",
+    },
+  });
+
+  console.log(admin);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+  });
